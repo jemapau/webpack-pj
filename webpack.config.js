6496936d34289b786a7ee4e4c3bbda532d55c2d0
Maria Paulina <je.mapau@gmail.com>
2017-06-04 15:04:53 -0500

    Handlebars

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
+const path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/main.js',
+    entry: [
+      'babel-polyfill',
+      './src/main.js',
+    ],
     output: {
-        path: __dirname + './dist',
+        path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js'
     },
     module: {
       rules: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          options: {
-            plugins: ['transform-runtime'],
-            presets: ['es2015']
-          }
-        }
+          { test: /\.jsx?$/, loader:'babel-loader', exclude: /node_modules/,
+            options: { plugins: ['transform-runtime'], presets: ['es2015']}
+          },
+          {test: /\.hbs$/, loader: 'handlebars-loader'}
       ]
-    }
+    },
+    plugins: [
+      new HtmlwebpackPlugin({
+        title: 'Intro to Webpack',
+        template: 'src/index.html'
+      })
+    ]
 };
